60a5973c5fcce34e68dadb5bab67a30ade1ac9a7
Braxton Young <youngbraxton@southern.edu>
2019-07-08 11:24:58 -0400

    Full color

--- v0001/Displays/fullColor.js
+++ v0001/Displays/fullColor.js
@@ -1,10 +1,19 @@
 var fullColor_currentColor = "#555555";
 var fullColor_volume = 0;
+var fullColor_colorArray = [];
+var fullColor_colorIndex = 0
 
 
 function fullColorInit(colors, volume){
+    fullColor_colorArray = colors;
 
-    fullColor_currentColor = selectColorFromArray(colors)
+    if(fullColor_colorIndex > (fullColor_colorArray.length - 2)){
+        fullColor_colorIndex = 0;
+    }else{
+        fullColor_colorIndex += 1;
+    }
+
+    fullColor_currentColor = fullColor_colorArray[fullColor_colorIndex]
     fullColor_volume = volume;
 
 }
